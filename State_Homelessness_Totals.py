0d7eed6761dc1d2dea1a4953761d47c7114641a2
njdevengine <newjerseydevelopment@gmail.com>
2019-05-17 15:07:54 -0400

    Update State_Homelessness_Totals.py

--- State_Homelessness_Totals.py
+++ State_Homelessness_Totals.py
@@ -22,3 +22,43 @@ n = 2018
 for i in range(1,len(data)):
     df[str(n)] = (list(data[i].iloc[:,2]))
     n-=1
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import pandas as pd
+
+for col in df.columns:
+    df[col] = df[col].astype(str)
+
+df['text'] = df['state']
+
+data = [go.Choropleth(
+    colorscale = scl,
+    autocolorscale = True,
+    locations = df['state'],
+    z = df['2018'].astype(float),
+    locationmode = 'USA-states',
+    text = df['text'],
+    marker = go.choropleth.Marker(
+        line = go.choropleth.marker.Line(
+            color = 'rgb(255,255,255)',
+            width = 2
+        )),
+    colorbar = go.choropleth.ColorBar(
+        title = "title goes here")
+)]
+
+layout = go.Layout(
+    title = go.layout.Title(
+        text = 'Homelessness by State<br>(Hover for breakdown)'
+    ),
+    geo = go.layout.Geo(
+        scope = 'usa',
+        projection = go.layout.geo.Projection(type = 'albers usa'),
+        showlakes = True,
+        lakecolor = 'rgb(255, 255, 255)'),
+)
+
+fig = go.Figure(data = data, layout = layout)
+py.iplot(fig, filename = 'd3-cloropleth-map')
